07746d1686010a3808de4d63d50e307b7f0a5c21
IAmHarshit0 <iamharshit237@gmail.com>
2026-08-12 16:09:55 +0000

    feat: Add Ingestion

---
+++ src/ingestion.py
@@ -0,0 +1,41 @@
+import json
+from pathlib import Path
+
+import pymupdf
+
+
+def extract_pdf(pdf_path: Path) -> dict:
+    doc = pymupdf.open(pdf_path)
+
+    pdf_data = {
+        "filename": pdf_path.name,
+        "numberOfPages": len(doc),
+        "pages": [],
+    }
+
+    for page_num, page in enumerate(doc):
+        page_text = page.get_text("text", sort=True)
+        text_lines = page_text.splitlines()
+
+        pdf_data["pages"].append({
+            "page_number": page_num + 1,
+            "text": text_lines,
+        })
+
+    doc.close()
+
+    return pdf_data
+
+
+def save_json(data: dict, output_path: Path) -> None:
+    with open(output_path, "w", encoding="utf-8") as out:
+        json.dump(data, out, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    pdf_path = Path("../data/doc.pdf")
+    output_path = Path("../data/output.json")
+
+    data = extract_pdf(pdf_path)
+    save_json(data, output_path)
+
+    print(f"Extracted {data['numberOfPages']} pages.")
---
+++ test/test_ingestion.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from src.ingestion import extract_pdf
+
+pdf_path = Path("data/doc.pdf")
+data = extract_pdf(pdf_path)
+
+def test_noOfPages():
+    assert data["numberOfPages"] == len(data["pages"])
+
+def test_structure():
+    for page in data["pages"]:
+        assert "page_number" in page
+        assert "text" in page
+        assert isinstance(page["page_number"], int)
+        assert isinstance(page["text"], list)
+
+def test_pageNumber():
+    pageNumbers = [
+        page["page_number"] 
+        for page in data["pages"]
+    ]
+    assert pageNumbers == list(range(1, data["numberOfPages"]+1))
+
+def test_pageText():
+    for page in data["pages"]:
+        assert len(page["text"]) > 0
+    
